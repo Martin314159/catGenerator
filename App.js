c5387030ff09571b36757dceaa5a0b050c8aeb12
Martin Reboutier <martinreboutier@macbook-pro-de-martin-2.home>
2022-11-03 18:36:32 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [img, setImg] = useState(' ')
+
+  const getCat = () => {
+    fetch('https://aws.random.cat/meow')
+    .then((res) => {
+      return res.json() 
+    }).then((data) => {
+      setImg(data.file)
+      console.log(img)
+    })
+  }
+
+  useEffect(() => {
+      getCat()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{uri: img}}
+        style={styles.img}
+      />
+      <Button 
+      onPress={getCat}
+      title="Nouveau chat"
+      color="#841584"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +43,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: '80%',
+    height: '75%',
+    borderRadius: '10',
+    marginBottom: '5%'
+  }
 });
